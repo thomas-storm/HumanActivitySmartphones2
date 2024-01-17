bcea65735e572c6e1b8730fbb0e68721f192af25
Thomas Storm <91027353+thomas-storm@users.noreply.github.com>
2024-01-17 14:20:02 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -89,7 +89,7 @@ Tidy <- Untidy %>%  select(matches("experiment") | matches("subject") | matches(
 
 Tidy_Mean <- Tidy %>% group_by(subject, activity) %>% summarize_at(c(4:67), mean, na.rm = TRUE)
 
-# write to two files
+# write to a text file
 
 write.table(Tidy_Mean, file = "./Tidy_Mean.txt", row.names = FALSE)
 
