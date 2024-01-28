22de7e0a165688915d96fd1044d6c3558e718cfe
thomas-storm <dr.storm@web.de>
2024-01-28 16:55:56 +0100

    chnages to barplot

--- plot1.R
+++ plot1.R
@@ -26,12 +26,62 @@ SCC <- readRDS("./data/Source_Classification_Code.rds")
 
 ## group data by year
 
-NEI_overall <- NEI %>% select(Emissions, year) %>% group_by(year)
+NEI_overall <- NEI %>%
+              select(Emissions, year) %>%
+              group_by(year) %>%
+              mutate(total.pm25 = mean(Emissions), sd.total.pm25 = sd(Emissions)) %>%
+              summarise(total.pm25, sd.total.pm25)
 
-## simple boxplot to show that Emissions decrease from 1999 to 2008
+head(NEI_overall)
 
-boxplot(Emissions ~ year, data = NEI_overall, outline = FALSE)
+## simple boxplot to show that Emissions decrease from 1999 to 2008## Peer-graded Assignment: Exploratory Data Analysis Course Project 2
+## Plot1
 
+library(dplyr)
 
-               
-               
+## set main directory and "data" sub directory
+## requires working directory is set correctly
+main_dir <- getwd()
+sub_dir <- "data"
+
+## check if "data" sub-directory exists, if not download and unzip files in data folder
+if (!dir.exists(sub_dir)){
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  temp <- tempfile()
+  dir.create(paste(main_dir, sub_dir, sep ="/"))
+  download.file(url, temp)
+  unzip(zipfile = temp, exdir = file.path(paste(main_dir, sub_dir, sep ="/")))
+  unlink(c(temp))
+  rm(temp)
+} else {print("data available")}
+
+## read files
+
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+## group data by year
+
+NEI_overall <- NEI %>%
+  select(Emissions, year) %>%
+  group_by(year) %>%
+  mutate(total.pm25 = sum(Emissions))
+
+year <- unique(NEI_overall$year)
+total <- unique(NEI_overall$total.pm25)
+
+## simple barplot of sum of emissons to show decrease from 1999 to 2008
+
+png(file = "plot1.png")
+
+barplot(total/10^6, year, 
+                ylim = c(0, 10),
+                names.arg = c("1999", "2002", "2005", "2008"),
+                main = "Total pm2.5 emissions 1999 - 2008",
+                xlab = "Year",
+                ylab = "Emissions [mto]"
+                )
+
+dev.off()
+
+##rm(list = ls())
